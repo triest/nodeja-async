ed1e58c2189b2ba3c315c9e8feba634b00352384
triest <triest21@gmail.com>
2019-07-08 16:24:08 +0300

    fix await

--- async2.js
+++ async2.js
@@ -2,24 +2,24 @@
 
 function later() {
   return new Promise(function(resolve) {
-    setTimeout(resolve({ token: 'test' }), 250);
+    setTimeout(resolve({ token: "test" }), 250);
     //setTimeout(resolve, 250);
   });
-};
+}
 
 //later();
 
-console.log("1.Resolve token 'test'")
-later().then(v => console.log(v))
+console.log("1.Resolve token 'test'");
+later().then(v => console.log(v));
 
 function later2() {
   return new Promise(function(resolve) {
     resolve(5);
   });
-};
+}
 
-console.log("2.Resolve 5")
-later2().then(v => console.log(v))
+console.log("2.Resolve 5");
+later2().then(v => console.log(v));
 /*
 Вызов:
 later().then(v => console.log(v))
@@ -29,27 +29,23 @@ later().then(v => console.log(v))
 
 //1.Promise reslove =>5
 
-
-
 function later3() {
   return new Promise(function(resolve) {
     setTimeout(resolve(5), 250);
   });
-};
+}
 
-console.log("3.Resolve from 250ms")
-later3().then(v => console.log(v))
-
-
+console.log("3.Resolve from 250ms");
+later3().then(v => console.log(v));
 
 function later4() {
   return new Promise(function(resolve) {
     resolve(5);
   });
-};
+}
 
-console.log("4.Resolve from 250ms")
-later4().then(v => console.log(v))
+console.log("4.Resolve from 250ms");
+later4().then(v => console.log(v));
 
 //2. Promise reslove =>{token:5}
 
@@ -57,21 +53,16 @@ function later5() {
   return new Promise(function(resolve) {
     setTimeout(resolve({ token: 5 }), 250);
   });
-};
+}
 
-console.log("5.Resolve token from 250ms")
-later5().then(v => console.log(v))
-
+console.log("5.Resolve token from 250ms");
+later5().then(v => console.log(v));
 
 function later6() {
   return new Promise(function(resolve) {
-   resolve({ token: 5 });
+    resolve({ token: 5 });
   });
-};
-
-
-
-
+}
 
 /*4.  С рандомом*/
 
@@ -79,46 +70,36 @@ function later7() {
   return new Promise(function(reject, resolve) {
     let rand = Math.random();
     if (rand > 0.5) {
-      setTimeout(resolve('ok'), 250);
+      setTimeout(resolve("ok"), 250);
     } else {
-      setTimeout(reject(new Error('Unknow error')), 250);
+      setTimeout(reject(new Error("Unknow error")), 250);
     }
   });
 }
 
-
-console.log("7.Return rando error")
-later7().then(v => console.log(v)).catch(v => console.log(v))
-
-
-
-
-
+console.log("7.Return rando error");
+later7()
+  .then(v => console.log(v))
+  .catch(v => console.log(v));
 
 //5.Resolve через 250мс со значением "token example"
 
 function func() {
-  console.log("token example")
+  console.log("token example");
 }
 
 //setTimeout(func(), 250);
-
-
 
 //6. Обработка ошибок в промис
 
 function later9() {
   return new Promise(function(resolve, reject) {
-    reject(new Error('Unknow error1'));
+    reject(new Error("Unknow error1"));
   });
-};
+}
 
 //p.catch(alert);
 
 //или
-later9().catch(console.log("error"))
+later9().catch(console.log("error"));
 //p.then(null, console.log("error"))
-
-
-
-
